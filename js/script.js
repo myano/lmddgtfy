0957a3d7aa4b830ec3e3ffa6eabdfff53be45e95
Zeke Y <zeke@zystvan.com>
2015-10-31 11:34:43 -0400

    Switch to using localStorage instead of cookies for the theme

--- js/script.js
+++ js/script.js
@@ -16,31 +16,12 @@ $(document).ready(function() {
     this.select();
   });
 
-  // get a cookie's value
-  // http://stackoverflow.com/a/4825695/4013202
-  function getCookie(c_name) {
-    if (document.cookie.length > 0) {
-      c_start = document.cookie.indexOf(c_name + "=");
-
-      if (c_start != -1) {
-        c_start = c_start + c_name.length + 1;
-        c_end = document.cookie.indexOf(";", c_start);
-
-        if (c_end == -1) {
-          c_end = document.cookie.length;
-        }
-        return unescape(document.cookie.substring(c_start, c_end));
-      }
-    }
-    return "";
-  }
-
-  if (getCookie('theme') == 'dark') {
+  if (localStorage["theme"] == "dark") {
     $('body').addClass('dark-theme').removeClass('light-theme');
     $('#ddg-logo').attr('src', 'img/ddg-logo-dark-theme.png');
     $('#ddg-theme-url-param').attr("value", "d");
   }
-  else if (getCookie('theme') == 'light') {
+  else if (localStorage["theme"] == "light") {
     $('body').addClass('light-theme').removeClass('dark-theme');
     $('#ddg-logo').attr('src', 'img/ddg-logo-light-theme.png');
     $('#ddg-theme-url-param').attr("value", "-1");
@@ -207,7 +188,7 @@ $(document).ready(function() {
     $('#ddg-logo').attr('src', 'img/ddg-logo-dark-theme.png');
     $('#ddg-theme-url-param').attr("value", "d");
 
-    document.cookie = 'theme=dark; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/;';
+    localStorage["theme"] = "dark";
   });
 
   $('#light-theme-icon').click(function() {
@@ -215,6 +196,6 @@ $(document).ready(function() {
     $('#ddg-logo').attr('src', 'img/ddg-logo-light-theme.png');
     $('#ddg-theme-url-param').attr("value", "-1");
 
-    document.cookie = 'theme=light; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/;';
+    localStorage["theme"] = "light";
   });
 });
